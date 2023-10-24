ce3ab823b65ce5e242be6193934d5f537c64f0c9
arefab00 <fabricio.arendtorres@unibas.ch>
2023-10-24 15:43:58 +0200

    added cpila to package lvl

--- enflows/nn/nets/__init__.py
+++ enflows/nn/nets/__init__.py
@@ -1,7 +1,7 @@
 from enflows.nn.nets.mlp import MLP, FCBlock
 from enflows.nn.nets.resnet import ConvResidualNet, ResidualNet
 from enflows.nn.nets.activations import Swish, CLipSwish, LeakyLSwish, FullSort, LipschitzCube, MaxMin, Sin, CSin, \
-    LipSwish
+    LipSwish, CPila
 from enflows.nn.nets.invertible_densenet import (DenseNet,
                                                  MixedConditionalDenseNet,
                                                  InputConditionalDenseNet,
