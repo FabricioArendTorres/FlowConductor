a7be716100d61fce868bbade265a4d72bdf063b1
Fabricio Arend Torres <9096900+FabricioArendTorres@users.noreply.github.com>
2024-02-22 18:22:42 +0100

    added setuptools requirement

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
         "Operating System :: OS Independent",
         "Development Status :: 2 - Pre-Alpha "
     ],
-    # python_requires='<3.11',
+    python_requires='<3.11',
     dependency_links=[],
 )
