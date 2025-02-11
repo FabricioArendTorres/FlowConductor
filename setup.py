df3f541fe385f7e4199588d3df0e6f9079b72dcd
Fabricio Arend Torres <9096900+FabricioArendTorres@users.noreply.github.com>
2025-02-11 16:26:27 +0000

    chore: development environment with vscode

--- setup.py
+++ setup.py
@@ -1,47 +1,47 @@
 from os import path
+
 from setuptools import find_packages, setup
 
-
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
+
+extras_require = {
+    "dev": [
+        "torchtestcase",
+        "pytest",
+        "pytest-cov",
+        "ruff",
+        "parameterized",
+    ],
+    "examples": ["matplotlib", "scikit-learn", "pandas"],
+}
+
+extras_require["all"] = sum(extras_require.values(), [])
+
+
 setup(
     name="flowcon",
-    version="0.0.4",
+    version="1.0.0",
     description="Normalizing flows in PyTorch. An extension of nflows.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url="https://github.com/FabricioArendTorres/FlowConductor/",
     author="Fabricio Arend Torres, Marcello Massimo Negri, Jonathan Aellen",
     packages=find_packages(exclude=["tests"]),
     license="MIT",
-    # setup_requires=['pbr'],
     install_requires=[
-        "matplotlib",
         "numpy",
-        "pandas",
         "torch",
-        "torchdiffeq",
-        "umnn",
-        "tqdm",
         "ninja",
-        "scikit-learn",
         "h5py",
-        "torchtestcase",
-        "parameterized",
-        # testing
-        "flake8",
-        "pytest",
-        "pytest-cov",
-        "black"
     ],
+    extras_require=extras_require,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Development Status :: 2 - Pre-Alpha "
+        "Development Status :: 2 - Pre-Alpha ",
     ],
-    # python_requires='>3.7',
     dependency_links=[],
 )
-
