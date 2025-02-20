d1dd6b8aa70daded96528f5f7c2aaaf352101190
Fabricio Arend Torres <9096900+FabricioArendTorres@users.noreply.github.com>
2025-02-20 16:27:09 +0000

    chore: typehints

--- flowcon/transforms/unitvector.py
+++ flowcon/transforms/unitvector.py
@@ -1,21 +1,28 @@
+from __future__ import annotations
+
 import torch
-from flowcon.transforms.base import Transform, InputOutsideDomain
+
+from flowcon.transforms.base import InputOutsideDomain, Transform
 
 
 class UnitVector(Transform):
     """
-    Bijective map from R^d to the surface of a sphere in R^(d+1) with an inverse stereographic projection.
+    Bijective map from R^d to the surface of a sphere in R^(d+1) with an inverse
+    stereographic projection.
+
     Inspired by
     https://www.tensorflow.org/probability/api_docs/python/tfp/bijectors/UnitVector
 
     """
 
-    def __init__(self, features):
+    def __init__(self, features: int):
         super().__init__()
         self.dim_Rd = features
         self.dim_sphere = torch.nn.Parameter(torch.tensor(features + 1, dtype=torch.float32))
 
-    def forward(self, inputs, context=None):
+    def forward(
+        self, inputs: torch.Tensor, context: torch.Tensor | None = None
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         """
         R^d to surface of sphere
         """
@@ -26,7 +33,9 @@ class UnitVector(Transform):
         output = torch.concatenate([2 * inputs, norm_sq - 1], dim=-1) / (norm_sq + 1)
         return output, self.forward_logabsdet(inputs).view(-1)
 
-    def inverse(self, inputs, context=None):
+    def inverse(
+        self, inputs: torch.Tensor, context: torch.Tensor | None = None
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         """
         Surface of sphere to R^d
         """
@@ -36,16 +45,24 @@ class UnitVector(Transform):
         outputs = (inputs / (1 - inputs[..., -1:]))[..., :-1]
         return outputs, -self.forward_logabsdet(outputs).view(-1)
 
-    def check_inverse_range(self, inverse_inputs):
-        if torch.abs(torch.max(self.norm_sq(inverse_inputs)) - 1.) > 1e-4:
+    def check_inverse_range(self, inverse_inputs: torch.Tensor) -> None:
+        if torch.abs(torch.max(self.norm_sq(inverse_inputs)) - 1.0) > 1e-4:
             raise InputOutsideDomain()
 
-    def forward_logabsdet(self, inputs):
+    def forward_logabsdet(self, inputs: torch.Tensor) -> torch.Tensor:
         """
         sqrt{det{J^T J}} = (2 / (sum^2 + 1))^n.
-        :param inputs:
-        :param forward_norm_sq:
-        :return:
+
+
+        Parameters
+        ----------
+        inputs : torch.Tensor
+            _description_
+
+        Returns
+        -------
+        torch.Tensor
+            _description_
         """
         n = self.dim_Rd
         log_2 = torch.log(torch.tensor(2).to(inputs))
@@ -53,5 +70,18 @@ class UnitVector(Transform):
         return n * (log_2 - torch.log1p(self.norm_sq(inputs).squeeze()))
 
     @staticmethod
-    def norm_sq(x):
-        return torch.sum(x ** 2, dim=-1, keepdim=True)
+    def norm_sq(x: torch.Tensor) -> torch.Tensor:
+        """
+        Calculate the squared norm over the last axis of the provided tensor.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            input tensor, of shape [..., d], with d being the dimension summed over.
+
+        Returns
+        -------
+        torch.Tensor
+            Squared norm of shape [..., d], same as the input shape.
+        """
+        return torch.sum(x**2, dim=-1, keepdim=True)
